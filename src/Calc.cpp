57c9f4095790309e870d25bdaa20f985229de28c
Marco Bassaletti <bassaletti@gmail.com>
2023-04-28 23:54:48 -0700

    [Calc] Dump input expression tokens

--- src/Calc.cpp
+++ src/Calc.cpp
@@ -1,3 +1,4 @@
+#include "Lexer.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/raw_ostream.h"
@@ -7,9 +8,19 @@ static llvm::cl::opt<std::string>
           llvm::cl::desc("<input expression>"),
           llvm::cl::init(""));
 
-int main(int argc, const char **argv) {
+int main(int argc, const char **argv)
+{
   llvm::InitLLVM X(argc, argv);
   llvm::cl::ParseCommandLineOptions(
       argc, argv, "calc - the expression compiler\n");
+
+  Lexer Lex(Input);
+  Token tok;
+  Lex.next(tok);
+  while (!tok.is(Token::TokenKind::EOI))
+  {
+    llvm::outs() << "Token: " << static_cast<int>(tok.getKind()) << " " << tok.getText() << "\n";
+    Lex.next(tok);
+  }
   return 0;
 }
